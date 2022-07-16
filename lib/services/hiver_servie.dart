927efa80155e74cb0c244dae9a8b5438310829e1
Mohammad <mnaseri841@gmail.com>
2022-07-16 15:32:55 +0430

    fixed init function

--- lib/services/hiver_servie.dart
+++ lib/services/hiver_servie.dart
@@ -2,8 +2,8 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:hive/hive.dart' as hive;
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_db_app/controllers/hive_controller.dart';
 import 'package:hive_db_app/models/person_model.dart';
 import 'package:path_provider/path_provider.dart' as path;
@@ -12,12 +12,11 @@ class HiveService {
   HiveService._();
   static final _ins = HiveService._();
   static HiveService get instance => _ins;
+  final adapter = PersonAdapter();
 
   var find = Get.find<HiveController>();
   Future<void> init() async {
-    Platform.isAndroid || Platform.isIOS
-        ? hive.Hive.init(await dirctory)
-        : null;
+    Platform.isAndroid || Platform.isIOS ? await hive.Hive.initFlutter() : null;
     find.isInitialized.value = true;
     return;
   }
@@ -29,12 +28,22 @@ class HiveService {
       });
 
   /// opens the person box with given name
-  Future<void> openPersons(String boxName) async {
-    var b = await hive.Hive.openBox<Person>(boxName);
+  Future<void> openPersons() async {
+    var b = await hive.Hive.openBox<Person>('persons');
     find.personBox = b.obs;
     find.update();
 
-    log('created box $boxName');
+    log('opened box ${b.name}');
     return;
   }
+
+  //TODO: Write Persons
+  Future<void> writePerosn(Person person) async {
+    await find.personBox.value!.add(person);
+    log('added ${person.name}');
+    find.update();
+  }
+  //TODO: Read Persons
+  //TODO: Update Persons
+  //TODO: Delete Persons
 }
