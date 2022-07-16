09ecacf43ab02f0b83bb0c05e01a6c9fc3086450
Mohammad <mnaseri841@gmail.com>
2022-07-16 15:34:50 +0430

    added some fields to the controller

--- lib/controllers/hive_controller.dart
+++ lib/controllers/hive_controller.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_db_app/models/person_model.dart';
 
 import '../services/hiver_servie.dart';
 
-class HiveController extends GetxController {
+class HiveController extends GetxController
+    with GetSingleTickerProviderStateMixin {
+  //
+
+  //
+  //keys
   var key = GlobalKey<ScaffoldState>();
+  var key1 = GlobalKey<ScrollableState>();
+  var key2 = GlobalKey<ScrollableState>();
+  //
+  //controllers
+  late final Rx<TabController> tabController;
+  late RxInt index;
+  final TextEditingController tc1 = TextEditingController();
+  final TextEditingController tc2 = TextEditingController();
+  final sc1 = ScrollController();
+  //
+  //hive
   RxBool isInitialized = false.obs;
-  Rx<Box?> box = null.obs;
+  Rx<Box<Person>?> personBox = null.obs;
+  Rx<Color> color = Colors.red.obs;
+
   @override
   void onInit() {
     // TODO: implement onInit
+    tabController = TabController(length: 2, vsync: this, initialIndex: 0).obs;
+    index = tabController.value.index.obs;
     super.onInit();
   }
 
@@ -18,7 +39,9 @@ class HiveController extends GetxController {
   void onReady() {
     // TODO: implement onReady
 
-    HiveService.instance.init();
+    HiveService.instance.openPersons();
+    color.value = Colors.green;
+    // key.currentState!.build(key.currentState!.context);
     super.onReady();
   }
 }
